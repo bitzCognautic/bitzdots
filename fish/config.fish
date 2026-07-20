a8add82600cf86659a97d4d998341cceaf79b09e
bitzCognautic <bitz.cognautic.@gmail.com>
2026-07-20 10:19:46 +0530

    fish: add ytdlpmp3/ytdlpmp4 aliases with auto mkdir

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,6 @@ end
 
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
+
+alias ytdlpmp3='mkdir -p ~/Musics; yt-dlp -x --audio-format mp3 --audio-quality 0 -o ~/Musics/"%(title)s.%(ext)s"'
+alias ytdlpmp4='mkdir -p ~/Musics; yt-dlp -f "bestvideo+bestaudio" --merge-output-format mp4 -o ~/Musics/"%(title)s.%(ext)s"'
